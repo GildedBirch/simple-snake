c29ce6c625c7ace5dae2eac987a231894cf96356
GildedBirch <150861088+GildedBirch@users.noreply.github.com>
2025-01-16 13:32:20 +0200

    Updated main.gd

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,7 +2,7 @@ extends Node
 
 @export var snake: Array[Vector2i]
 
-class TILE:
+class Tile:
 	const FLOOR := Vector2i(1,1)
 	const SNAKE := Vector2i(1,3)
 	const HEAD := Vector2i(0, 3)
@@ -15,7 +15,7 @@ var prev_dir := Vector2i.UP
 func _ready() -> void:
 	draw_snake(Vector2i(-1,-1))
 
-func _input(_event: InputEvent) -> void:
+func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed(&"up"):
 		dir = Vector2i.UP
 	if Input.is_action_just_pressed(&"down"):
@@ -24,43 +24,56 @@ func _input(_event: InputEvent) -> void:
 		dir = Vector2i.LEFT
 	if Input.is_action_just_pressed(&"right"):
 		dir = Vector2i.RIGHT
+	if  Input.is_action_just_pressed(&"close"):
+		get_tree().quit()
 
 func move(move_dir: Vector2i):
-	if move_dir == -prev_dir: move_dir = prev_dir
+	# Prevent snake from moving back to itself
+	if move_dir == -prev_dir:
+		move_dir = prev_dir
 	prev_dir = move_dir
 	
 	var ate := false
+	# Position where head will be next
 	var next_pos: Vector2i = snake[0] + move_dir
+	# Position where tail was
+	var clear_pos: Vector2i = snake[-1]
 	
-	var next_tile_data: TileData = %TileMap.get_cell_tile_data(next_pos)
-	if next_tile_data.get_custom_data("death"):
-		get_tree().quit()
+	# Check collision with walls, self, or food
+	var next_Tile_data: TileData = %TileMap.get_cell_tile_data(next_pos)
+	if next_Tile_data.get_custom_data("death"):
+		get_tree().reload_current_scene()
 		return
-	elif next_tile_data.get_custom_data("interactable"):
-		snake.append(snake[-1])
+	elif next_Tile_data.get_custom_data("interactable"):
+		snake.append(clear_pos)
+		# Don't clear anything this round if we ate, so we can add new tail
+		clear_pos = Vector2i(-1, -1)
 		ate = true
 
-	for i in range(snake.size()):
-		var new_pos: Vector2i = next_pos
-		next_pos = snake[i]
-		snake[i] = new_pos
-	draw_snake(next_pos)
-	if ate: spawn_food()
+	# Move
+	for i in range(-1, -snake.size(), -1):
+		snake[i] = snake[i-1]
+	snake[0] = next_pos
+
+	# Draw snake
+	draw_snake(clear_pos)
+	
+	# Spawn new food
+	if ate:
+		spawn_food()
 
 func draw_snake(clear_pos: Vector2i):
-	for snake_pos: Vector2i in snake:
-		%TileMap.set_cell(snake_pos, 0, TILE.SNAKE)
-	%TileMap.set_cell(snake[0], 0, TILE.HEAD)
-	%TileMap.set_cell(clear_pos, 0, TILE.FLOOR)
+	%TileMap.set_cell(snake[0], 0, Tile.HEAD)
+	%TileMap.set_cell(snake[1], 0, Tile.SNAKE)
+	# Clear tail only if we didn't eat
+	if clear_pos != Vector2i(-1, -1):
+		%TileMap.set_cell(clear_pos, 0, Tile.FLOOR)
 
+# Spawn new food by getting all emtpy floor tiles
 func spawn_food() -> void:
-	var area := []
-	for y in range(1,6):
-		for x in range(1,8):
-			var pos := Vector2i(x, y)
-			if not snake.has(pos):
-				area.append(pos)
-	%TileMap.set_cell(area.pick_random(), 0, TILE.FOOD)
+	var cells = %TileMap.get_used_cells_by_id(0, Vector2i(1, 1))
+	%TileMap.set_cell(cells.pick_random(), 0, Tile.FOOD)
 
+# Move every tick
 func _on_timer_timeout() -> void:
 	move(dir)
